265fde08913f44a43161688a54ca22fe0a62a4c9
Rahbar22 <rahbarali307@gmail.com>
2024-06-21 04:10:39 +0530

    Commit

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ if(process.env.NODE_ENV==='PRODUCTION'){
     app.use(express.static(path.join(__dirname, "../frontend/build")));
 
     app.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+        res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
     });
 }
 
